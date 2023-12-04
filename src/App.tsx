85cff5c1ace14a1e45882c7a8510f75be478d883
tkkwa01 <aoisyanok@gmail.com>
2023-12-04 17:51:41 +0900

    goのバックエンドとの接続テスト完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { MessageInput } from "./components/MessageInput";
+import { MessageList } from "./components/MessageList";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>Simple Chat</h1>
+        <MessageInput />
+        <MessageList />
+      </div>
   );
-}
-
-export default App;
+};
